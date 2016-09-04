7d2ba1d9066389b151053eceafd38d17f85be31b
Louis An <dan2@student.monash.edu>
2016-09-04 17:57:35 +1000

    Initial Commit

---
+++ Tic-Tac-Toe-AITests/Tic_Tac_Toe_AITests.swift
@@ -0,0 +1,36 @@
+//
+//  Tic_Tac_Toe_AITests.swift
+//  Tic-Tac-Toe-AITests
+//
+//  Created by Louis An on 4/09/2016.
+//  Copyright © 2016 Louis An. All rights reserved.
+//
+
+import XCTest
+@testable import Tic_Tac_Toe_AI
+
+class Tic_Tac_Toe_AITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
